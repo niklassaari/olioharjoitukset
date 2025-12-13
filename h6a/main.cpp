c0a56bc1f9d40cad0f751613c39a56ac7d0df4dc
Niklas <niklassaari04@gmail.com>
2025-12-13 15:56:30 +0200

    Viikko 6

---
+++ h6a/main.cpp
@@ -0,0 +1,11 @@
+#include "myclass.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    MyClass obj;
+    obj.raiseMySignal();
+    return 0;
+}
---
+++ h6b/main.cpp
@@ -0,0 +1,12 @@
+#include "exampleclass.h"
+#include <QCoreApplication>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    ExampleClass obj;
+    obj.startToWait();
+
+    return a.exec();
+}
